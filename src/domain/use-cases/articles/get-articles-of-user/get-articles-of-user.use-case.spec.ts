dd63e70fe0ee4d809654a2fc9d7425b1e6b34a6d
Leonardo Peixoto <leonardo.lima@saudeid.com.br>
2022-10-26 18:14:11 -0300

    test: add test to validate get articles of user

---
+++ src/domain/use-cases/articles/get-articles-of-user/get-articles-of-user.use-case.spec.ts
@@ -0,0 +1,21 @@
+import { IArticleRepository } from '@medium/domain/repositories/__mocks__/article.repository';
+import { GetArticlesOfUserUseCase } from '@medium/domain/use-cases/articles/get-articles-of-user/get-articles-of-user.use-case';
+
+describe('GetArticlesOfUserUseCase', () => {
+  describe('execute', () => {
+    it('should call "IArticleRepository.getAllArticlesOfUser" with correct params', async () => {
+      const page = 1;
+      const userId = 1;
+      const repository = new IArticleRepository();
+      (repository.getAllArticlesOfUser as jest.Mock).mockResolvedValue([]);
+
+      await new GetArticlesOfUserUseCase(repository).execute(userId, page);
+
+      expect(repository.getAllArticlesOfUser).toHaveBeenNthCalledWith(
+        1,
+        userId,
+        page,
+      );
+    });
+  });
+});
