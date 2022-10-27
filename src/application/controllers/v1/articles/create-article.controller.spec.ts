faac44257f5b5149ebc019dfa61bb9bba8e4b38e
Leonardo Peixoto <leonardo.lima@saudeid.com.br>
2022-10-27 13:36:51 -0300

    test: improve test of CreateArticleController

--- src/application/controllers/v1/articles/create-article.controller.spec.ts
+++ src/application/controllers/v1/articles/create-article.controller.spec.ts
@@ -5,23 +5,15 @@ import { CreateArticleController } from '@medium/application/controllers/v1/arti
 import { CreateArticleRequest } from '@medium/application/controllers/v1/articles/requests/create-article.request';
 import { CreateArticleResponse } from '@medium/application/controllers/v1/articles/responses/create-article.response';
 import { UserByIdPipe } from '@medium/application/pipe/user-by-id.pipe';
-import { Article } from '@medium/domain/entities/article';
-import { User } from '@medium/domain/entities/user';
+import { articleFactory } from '@medium/domain/entities/__mocks__/article.factory';
+import { userFactory } from '@medium/domain/entities/__mocks__/user.factory';
 import { CreateArticleUseCase } from '@medium/domain/use-cases/articles/create/create-article.use-case';
 
 jest.mock(
   '@medium/domain/use-cases/articles/create/create-article.use-case',
   () => ({
     CreateArticleUseCase: jest.fn().mockImplementation(() => ({
-      execute: jest
-        .fn()
-        .mockResolvedValue(
-          new Article(
-            faker.datatype.string(),
-            faker.datatype.string(),
-            new User(faker.name.fullName(), faker.internet.email()),
-          ),
-        ),
+      execute: jest.fn().mockResolvedValue(articleFactory()),
     })),
   }),
 );
@@ -44,17 +36,12 @@ describe('CreateArticleController', () => {
   describe('create', () => {
     it('should return an instance of CreateArticleResponse', () => {
       expect(
-        module
-          .get(CreateArticleController)
-          .create(
-            new User(faker.name.fullName(), faker.internet.email()),
-            request,
-          ),
+        module.get(CreateArticleController).create(userFactory(), request),
       ).resolves.toBeInstanceOf(CreateArticleResponse);
     });
 
     it('should call "CreateArticleUseCase.execute" with an instance of toCreateArticleInputData', async () => {
-      const user = new User(faker.name.fullName(), faker.internet.email());
+      const user = userFactory();
 
       await module.get(CreateArticleController).create(user, request);
 
