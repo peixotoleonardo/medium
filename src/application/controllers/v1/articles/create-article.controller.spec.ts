509f0dba068b82a71f1dc82c3cde021446aadd95
Leonardo Peixoto <leonardo.lima@saudeid.com.br>
2022-10-27 11:42:30 -0300

    test: improve test of create article controller

--- src/application/controllers/v1/articles/create-article.controller.spec.ts
+++ src/application/controllers/v1/articles/create-article.controller.spec.ts
@@ -9,30 +9,32 @@ import { Article } from '@medium/domain/entities/article';
 import { User } from '@medium/domain/entities/user';
 import { CreateArticleUseCase } from '@medium/domain/use-cases/articles/create/create-article.use-case';
 
+jest.mock(
+  '@medium/domain/use-cases/articles/create/create-article.use-case',
+  () => ({
+    CreateArticleUseCase: jest.fn().mockImplementation(() => ({
+      execute: jest
+        .fn()
+        .mockResolvedValue(
+          new Article(
+            faker.datatype.string(),
+            faker.datatype.string(),
+            new User(faker.name.fullName(), faker.internet.email()),
+          ),
+        ),
+    })),
+  }),
+);
+
 describe('CreateArticleController', () => {
   let module: TestingModule;
-  const user = new User(faker.name.fullName(), faker.internet.email());
-  const request = new CreateArticleRequest(
-    faker.lorem.text(),
-    faker.lorem.words(),
-  );
-  const useCaseMock = {
-    execute: jest
-      .fn()
-      .mockResolvedValue(
-        new Article(faker.datatype.string(), faker.datatype.string(), user),
-      ),
-  };
+  let request: CreateArticleRequest;
 
   beforeEach(async () => {
+    request = new CreateArticleRequest(faker.lorem.text(), faker.lorem.words());
     module = await Test.createTestingModule({
       controllers: [CreateArticleController],
-      providers: [
-        {
-          provide: CreateArticleUseCase,
-          useValue: useCaseMock,
-        },
-      ],
+      providers: [CreateArticleUseCase],
     })
       .overridePipe(UserByIdPipe)
       .useValue(jest.fn())
@@ -42,14 +44,21 @@ describe('CreateArticleController', () => {
   describe('create', () => {
     it('should return an instance of CreateArticleResponse', () => {
       expect(
-        module.get(CreateArticleController).create(user, request),
+        module
+          .get(CreateArticleController)
+          .create(
+            new User(faker.name.fullName(), faker.internet.email()),
+            request,
+          ),
       ).resolves.toBeInstanceOf(CreateArticleResponse);
     });
 
     it('should call "CreateArticleUseCase.execute" with an instance of toCreateArticleInputData', async () => {
+      const user = new User(faker.name.fullName(), faker.internet.email());
+
       await module.get(CreateArticleController).create(user, request);
 
-      expect(useCaseMock.execute).toHaveBeenNthCalledWith(
+      expect(module.get(CreateArticleUseCase).execute).toHaveBeenNthCalledWith(
         1,
         request.toCreateArticleInputData(user),
       );
