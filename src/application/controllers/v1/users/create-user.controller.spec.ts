0d5d088b6e79daf43078acac4e3db6a8171785bc
Leonardo Peixoto <leonardo.lima@saudeid.com.br>
2022-10-27 15:26:49 -0300

    test: improve test of CreateUserController

--- src/application/controllers/v1/users/create-user.controller.spec.ts
+++ src/application/controllers/v1/users/create-user.controller.spec.ts
@@ -1,46 +1,32 @@
-import { faker } from '@faker-js/faker';
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { CreateUserController } from '@medium/application/controllers/v1/users/create-user.controller';
-import { CreateUserRequest } from '@medium/application/controllers/v1/users/requests/create-user.request';
+import { createUserRequestFactory } from '@medium/application/controllers/v1/users/requests/__mocks__/create-user.request.factory';
 import { CreateUseResponse } from '@medium/application/controllers/v1/users/responses/create-user.response';
-import { User } from '@medium/domain/entities/user';
 import { CreateUserInputData } from '@medium/domain/use-cases/users/create/create-user.input-data';
 import { CreateUserUseCase } from '@medium/domain/use-cases/users/create/create-user.use-case';
 
+jest.mock('@medium/domain/use-cases/users/create/create-user.use-case');
+
 describe('CreateUserController', () => {
   let module: TestingModule;
-  const request = new CreateUserRequest(
-    faker.name.fullName(),
-    faker.internet.email(),
-  );
 
   beforeEach(async () => {
     module = await Test.createTestingModule({
       controllers: [CreateUserController],
-      providers: [
-        {
-          provide: CreateUserUseCase,
-          useValue: {
-            execute: jest
-              .fn()
-              .mockResolvedValue(
-                new User(faker.name.fullName(), faker.internet.email()),
-              ),
-          },
-        },
-      ],
+      providers: [CreateUserUseCase],
     }).compile();
   });
 
   describe('create', () => {
     it('should return a CreateUseResponse instance', () => {
       expect(
-        module.get(CreateUserController).create(request),
+        module.get(CreateUserController).create(createUserRequestFactory()),
       ).resolves.toBeInstanceOf(CreateUseResponse);
     });
 
     it('given a request should call "CreateUserUseCase.execute" with a valid CreateUserInputData', () => {
+      const request = createUserRequestFactory();
       module.get(CreateUserController).create(request);
 
       expect(module.get(CreateUserUseCase).execute).toHaveBeenNthCalledWith(
