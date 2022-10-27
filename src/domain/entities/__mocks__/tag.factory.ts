994f2e21082e0f4cefa4c126a485e85bcda6971a
Leonardo Peixoto <leonardo.lima@saudeid.com.br>
2022-10-27 18:28:07 -0300

    refactor: change type of argument

--- src/domain/entities/__mocks__/tag.factory.ts
+++ src/domain/entities/__mocks__/tag.factory.ts
@@ -2,5 +2,5 @@ import { faker } from '@faker-js/faker';
 
 import { Tag } from '@medium/domain/entities/tag';
 
-export const tagFactory = (tag?: Tag) =>
+export const tagFactory = (tag?: Partial<Tag>) =>
   new Tag(tag?.value ?? faker.datatype.string());
