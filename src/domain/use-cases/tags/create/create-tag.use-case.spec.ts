0e1ae43a1e50f44c40b7043b008eef7d614226da
Leonardo Peixoto <leonardo.lima@saudeid.com.br>
2022-10-27 18:31:06 -0300

    test: add test to CrateTagUseCase

---
+++ src/domain/use-cases/tags/create/create-tag.use-case.spec.ts
@@ -0,0 +1,36 @@
+import { tagFactory } from '@medium/domain/entities/__mocks__/tag.factory';
+import { Tag } from '@medium/domain/entities/tag';
+import { TagRepository } from '@medium/domain/repositories/__mocks__/tag.repository';
+import { CreateTagUseCase } from '@medium/domain/use-cases/tags/create/create-tag.use-case';
+
+describe('CrateTagUseCase', () => {
+  const tagRepositoryMock = new TagRepository();
+  const usecase = new CreateTagUseCase(tagRepositoryMock);
+
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  describe('execute', () => {
+    it('given an empty array of tags should return an empty array', () => {
+      expect(usecase.execute([])).resolves.toStrictEqual([]);
+    });
+
+    it('should save only tags that not exist in database', async () => {
+      const tags = ['not-to-save', 'save'];
+      (tagRepositoryMock.findByValue as jest.Mock).mockResolvedValue([
+        tagFactory({ value: 'not-to-save' }),
+      ]);
+
+      await usecase.execute(tags);
+
+      expect(tagRepositoryMock.save).toHaveBeenNthCalledWith(1, [
+        new Tag('save'),
+      ]);
+    });
+  });
+});
